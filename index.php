969aeff902572746c21d0585eecb54e0ee09c0a7
Erfin Gustaman <gopin.ipin@gmail.com>
2023-03-20 09:01:29 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css" integrity="sha512-H9jrZiiopUdsLpg94A333EfumgUBpO9MdbxStdeITo+KEIMaNfHNvwyjjDJb+ERPaRS6DpyRlKbvPUasNItRyw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+<h1>alow</h1>
+<hr>
+<a href="plugins/filemanager/dialog.php?type=0" class="btn iframe-btn" type="button">Open Filemanager</a> -->
+
+<!-- <script src="https://code.jquery.com/jquery-latest.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css" integrity="sha512-H9jrZiiopUdsLpg94A333EfumgUBpO9MdbxStdeITo+KEIMaNfHNvwyjjDJb+ERPaRS6DpyRlKbvPUasNItRyw==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
+
+<?php
+    header("location: plugins/filemanager/dialog.php?type=0");
+?>
